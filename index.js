3b94c702e26945f77fece672a885c553f9d822e8
Genbel <Genbel@MacBook-Pro.local>
2016-06-15 17:43:50 +0100

    Local Storage
    We set a local storage for our TODO list

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import 'babel-polyfill'
 import React from 'react';
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
@@ -5,7 +6,23 @@ import { createStore } from 'redux'
 import todoApp from './reducers'
 import App from './components/App'
 
-let store = createStore(todoApp)
+import { loadState, saveState } from './localStorage';
+import throttle from 'lodash/throttle';
+
+// Sometimes we want to have a initial state already
+// Filled with something.
+// We if we set in the create store as a second argument,
+// we can pass there the initial state filled with something
+const persistedState = loadState();
+
+const store = createStore(todoApp, persistedState);
+
+// When the store change it will trigger subscribe and it will save in the localStoreage
+store.subscribe(throttle(() => {
+    saveState({
+        todos: store.getState().todos
+    });
+}), 1000);
 
 render(
   <Provider store={store}>
