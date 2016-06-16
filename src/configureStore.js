89bf08054bad17bfd3e4ca64f8d868d2bf09c650
Genbel <Genbel@MacBook-Pro.local>
2016-06-16 17:55:51 +0100

    Log the actions in the console

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,6 +3,24 @@ import todoApp from './reducers';
 import { loadState, saveState } from './localStorage';
 import throttle from 'lodash/throttle';
 
+const addLoggingToDispatch = (store) => {
+
+    const rawDispatch = store.dispatch;
+
+    if(!console.group){
+        return rawDispatch;
+    }
+    return (action) => {
+        console.group(action.type);
+        console.log('%c prev state \n','color: green', store.getState());
+        console.log('%c action \n', 'color: blue', action);
+        const returnValue = rawDispatch(action);
+        console.log('%c next state \n', 'color:red', store.getState());
+        console.groupEnd(action.type);
+        return returnValue;
+    }
+}
+
 const configureStore = () => {
 
     // Sometimes we want to have a initial state already
@@ -13,7 +31,10 @@ const configureStore = () => {
 
     const store = createStore(todoApp, persistedState);
 
-    
+    if(process.env.NODE_ENV !== 'production'){
+        // Overwrite the dispatch method
+        store.dispatch = addLoggingToDispatch(store);
+    }
 
     // When the store change it will trigger subscribe and it will save in the localStoreage
     store.subscribe(throttle(() => {
