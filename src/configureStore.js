758ad9ddb0d2e3e4d7a5f819a6a652f3205fd8bb
Genbel <Genbel@MacBook-Pro.local>
2016-06-16 18:13:45 +0100

    FakeDatabase

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,7 +1,5 @@
 import { createStore } from 'redux';
 import todoApp from './reducers';
-import { loadState, saveState } from './localStorage';
-import throttle from 'lodash/throttle';
 
 const addLoggingToDispatch = (store) => {
 
@@ -23,25 +21,12 @@ const addLoggingToDispatch = (store) => {
 
 const configureStore = () => {
 
-    // Sometimes we want to have a initial state already
-    // Filled with something.
-    // We if we set in the create store as a second argument,
-    // we can pass there the initial state filled with something
-    const persistedState = loadState();
-
-    const store = createStore(todoApp, persistedState);
+    const store = createStore(todoApp);
 
     if(process.env.NODE_ENV !== 'production'){
         // Overwrite the dispatch method
         store.dispatch = addLoggingToDispatch(store);
     }
-
-    // When the store change it will trigger subscribe and it will save in the localStoreage
-    store.subscribe(throttle(() => {
-        saveState({
-            todos: store.getState().todos
-        });
-    }), 1000);
 
     return store;
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import React from 'react';
 import { render } from 'react-dom'
 import Root from './components/Root'
 import ConfigureStore from './configureStore';
+import { fetchTodos } from './api';
+
+fetchTodos('all').then(todos =>
+    console.log(todos)
+);
 
 const store = ConfigureStore();
 
